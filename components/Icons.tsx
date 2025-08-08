3323471611c9cbd4312eaffebdfde57c9158a2bf
Farouk568-f <78809939+Farouk568-f@users.noreply.github.com>
2025-08-08 17:02:14 +0100

    Update Icons.tsx

--- components/Icons.tsx
+++ components/Icons.tsx
@@ -73,6 +73,18 @@ export const PipIcon: React.FC<IconProps> = (props) => (
     </svg>
 );
 
+export const Rewind10Icon: React.FC<IconProps> = (props) => (
+    <svg viewBox="0 0 24 24" fill="currentColor" {...props}>
+        <path d="M11.99 5V1l-5 5 5 5V7c3.31 0 6 2.69 6 6s-2.69 6-6 6-6-2.69-6-6h-2c0 4.42 3.58 8 8 8s8-3.58 8-8-3.58-8-8-8z"/>
+    </svg>
+);
+
+export const Forward10Icon: React.FC<IconProps> = (props) => (
+    <svg viewBox="0 0 24 24" fill="currentColor" {...props}>
+        <path d="M12 5V1L7 6l5 5V7c3.31 0 6 2.69 6 6s-2.69 6-6 6-6-2.69-6-6H4c0 4.42 3.58 8 8 8s8-3.58 8-8-3.58-8-8-8z"/>
+    </svg>
+);
+
 // Note: These icons are not simple paths and are kept for their specific functionality/style
 export const LikeIcon: React.FC<IconProps> = (props) => (
     <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24" focusable="false" {...props}>
